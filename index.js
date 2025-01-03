2c9a0021716ee6481c1b339123a6baba2899024c
jonasluis <jonasluis66@gmail.com>
2025-01-03 12:10:44 -0300

    add estilo dinamico na lista

--- index.js
+++ index.js
@@ -20,6 +20,15 @@ botaoAdicionar.addEventListener("click", (evento) => {
     const nomeItem= document.createElement("p")
     nomeItem.innerText = inputItem.value
 
+    inputCheckbox.addEventListener("click", function(){
+        if(inputCheckbox.checked) {
+            nomeItem.style.textDecoration = "line-through"
+        }
+        else{
+            nomeItem.style.textDecoration = "none"
+        }
+    })
+
     containerItemDaLista.appendChild(inputCheckbox)
     containerItemDaLista.appendChild(nomeItem)
     
