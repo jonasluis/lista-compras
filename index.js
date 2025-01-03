ce285471baba41fb7f31d6cbfd000e1838ac6f18
jonasluis <jonasluis66@gmail.com>
2025-01-03 11:36:24 -0300

    criado as funcoes de adicionar o valor do item digitado a uma lista

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const inputItem = document.getElementById("input-item")
-
+const listaDeCompras = document.getElementById("lista-de-compras")
 const botaoAdicionar = document.getElementById("adicionar-item")
+
+let contador = 0
 
 botaoAdicionar.addEventListener("click", (evento) => {
     evento.preventDefault() //nao deixa atualizar a tela
@@ -12,6 +14,15 @@ botaoAdicionar.addEventListener("click", (evento) => {
     const itemDaLista = document.createElement("li")
     const containerItemDaLista = document.createElement("div")
     containerItemDaLista.classList.add("lista-item-container")
+    const inputCheckbox = document.createElement("input")
+    inputCheckbox.type = "checkbox"
+    inputCheckbox.id = "checkbox-" + contador++
+    const nomeItem= document.createElement("p")
+    nomeItem.innerText = inputItem.value
 
+    containerItemDaLista.appendChild(inputCheckbox)
+    containerItemDaLista.appendChild(nomeItem)
 
+    itemDaLista.appendChild(containerItemDaLista)
+    listaDeCompras.appendChild(itemDaLista)
 })
