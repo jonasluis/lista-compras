6d9eb87caf25dd886220bb008db7bf10ff90dca0
jonasluis <jonasluis66@gmail.com>
2025-01-04 11:11:41 -0300

    add mensagem para quando a lista estiver vazia

--- index.js
+++ index.js
@@ -49,4 +49,20 @@ botaoAdicionar.addEventListener("click", (evento) => {
 
     listaDeCompras.appendChild(itemDaLista)
 
+    verificarListaVazia()
+
 })
+
+const mensagemListaVazia = document.querySelector(".mensagem-lista-vazia")
+
+function verificarListaVazia() {
+    const itensDaLista = listaDeCompras.querySelectorAll("li")
+
+    if(itensDaLista.length === 0) {
+        mensagemListaVazia.style.display = "block"
+    } else {
+        mensagemListaVazia.style.display = "none"
+
+    }
+}
+verificarListaVazia()
